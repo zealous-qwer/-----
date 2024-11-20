dbef04f73d5044b1c39a1490bb3f72d22309f500
王嘉瑞 <764969789@qq.com>
2024-11-20 13:23:16 +0800

    流程图2

--- import math.py
+++ import math.py
@@ -1,25 +1,24 @@
-def print_pascals_triangle(num_rows):
-    # 初始化一个空列表来存储杨辉三角形的每一行
-    triangle = []
- 
-    # 逐行生成杨辉三角形
-    for i in range(num_rows):
-        # 每一行开始都是一个1
-        row = [1] * (i + 1)
-        # 中间的元素是上方两个元素之和
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        # 将当前行添加到三角形中
-        triangle.append(row)
- 
-    # 打印杨辉三角形
-    max_width = len(" ".join(map(str, triangle[-1])))  # 计算最后一行的最大宽度
-    for row in triangle:
-        # 居中打印每一行，前面填充空格
-        print(" " * ((max_width - len(" ".join(map(str, row)))) // 2) + " ".join(map(str, row)))
- 
-# 设置要打印的行数
-num_rows = 10
- 
-# 打印杨辉三角形
-print_pascals_triangle(num_rows)
+import graphviz
+dot=graphviz.Digraph(comment='the round table',name="分支结构",node_attr={'shape':'box'})
+dot.node('1','开始')
+dot.node('2','请输入成绩=?', shape='parallelogram')
+dot.node('3','x是否大等于90?', shape='diamond')
+dot.node('4','x是否大等于80?', shape='diamond')
+dot.node('5','x是否大等于70?', shape='diamond')
+dot.node('6','x是否大等于60?', shape='diamond')
+dot.node('a','Print A')
+dot.node('b','Print B')
+dot.node('c','Print C')
+dot.node('d','Print D')
+dot.node('e','Print E')
+dot.node('7','结束')
+dot.edges(['12','23','a7','b7','c7','d7','e7'])
+dot.edge('3','a',label='Yes')
+dot.edge('3','4',label='No')
+dot.edge('4','b',label='Yes')
+dot.edge('4','5',label='No')
+dot.edge('5','c',label='Yes')
+dot.edge('5','6',label='No')
+dot.edge('6','d',label='Yes')
+dot.edge('6','e',label='No')
+dot
